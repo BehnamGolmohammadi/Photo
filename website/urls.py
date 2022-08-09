6a5d892a593fa8ec79b3649f9dd28d427b1d5501
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-09 12:00:28 +0430

    new path
    create new path for contact and about page and point them to their related views

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,7 @@ from website.views import *
 app_name= 'website'
 
 urlpatterns = [
-    path('', website_home, name='home')
+    path('', website_home, name='home'),
+    path('about', website_about, name='about'),
+    path('contact', website_contact, name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def website_home(request):
     return render(request, 'website/index.html')
+
+def website_about(request):
+    return render(request, 'website/about.html')
+
+def website_contact(request):
+    return render(request, 'website/contact.html')
