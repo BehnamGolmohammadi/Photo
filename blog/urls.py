7661961bda971afdb86693ca91966d169ff56832
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-09 12:23:07 +0430

    add blog home and post url
    also point it to its related view

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog.views import *
 app_name= 'blog'
 
 urlpatterns = [
-    path('', blog_home, name='home')
+    path('', blog_home, name='home'),
+    path('single', blog_post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_home(request):
-    return render(request, 'test.html')
+    return render(request, 'blog/blog.html')
+
+def blog_post(request):
+    return render(request, 'blog/single_post.html')
