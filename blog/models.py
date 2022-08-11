824f5548f99556fbbbfab22c8932629865e370b7
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-11 13:00:19 +0430

    update Post model

--- blog/models.py
+++ blog/models.py
@@ -1,27 +1,20 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-
 # Create your models here.
 
-def user_directory_path(instance, filename):
-  
-    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class Post(models.Model):
     Author = models.ForeignKey(User, on_delete= models.CASCADE)
     Title = models.CharField(max_length= 255)
     Slug = models.SlugField(max_length= 255)
     Content = models.TextField()
-    Image = models.ImageField(upload_to = user_directory_path)
+    Image = models.ImageField(upload_to = 'blog/', default = 'blog/Default.png')
     Status = models.BooleanField(default= False)
     # Tags = 
     # Category = 
-    Created_Date = models.TimeField( default = timezone.now)
-    Updated_Date = models.TimeField( default = timezone.now)
-    Published_Date = models.TimeField()
+    Created_Date = models.DateTimeField( auto_now_add= True)
+    Updated_Date = models.DateTimeField( auto_now= True)
+    Published_Date = models.DateTimeField(null= True)
 
     class META:
         pass
