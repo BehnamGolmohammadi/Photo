41616977b409f1dc1c879a8f232e18d46d8ac011
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-11 12:36:18 +0430

    Create Post model and config it in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Post
+
 
 # Register your models here.
+class Admin_PostCustomizations(admin.ModelAdmin):
+    date_hierarchy = "Created_Date"
+
+
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+def user_directory_path(instance, filename):
+  
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Post(models.Model):
+    Author = models.ForeignKey(User, on_delete= models.CASCADE)
+    Title = models.CharField(max_length= 255)
+    Slug = models.SlugField(max_length= 255)
+    Content = models.TextField()
+    Image = models.ImageField(upload_to = user_directory_path)
+    Status = models.BooleanField(default= False)
+    # Tags = 
+    # Category = 
+    Created_Date = models.TimeField( default = timezone.now)
+    Updated_Date = models.TimeField( default = timezone.now)
+    Published_Date = models.TimeField()
+
+    class META:
+        pass
+
+
+    def __str__(self):
+        return self.Title
