4207c979000d8055f2326d8a666b1c58f2cfca52
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-11 13:19:14 +0430

    Update showing info of posts in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,19 @@ from blog.models import Post
 # Register your models here.
 class Admin_PostCustomizations(admin.ModelAdmin):
     date_hierarchy = "Created_Date"
+    empty_value_display= 'No Information'
+    fieldsets = (
+        ('Main options', {
+            'fields': ("Author", "Image", "Title", "Content")
+        }),
+        ('Advanced options', {
+            'classes': ('wide', 'collapse', 'extrapretty'),
+            'fields': ("Status", "Published_Date",),
+        }),
+    )
+    list_display= ("Title", "Author" ,"Status", "Published_Date")
+    list_filter= ("Status", "Author", "Published_Date")
+    search_fields= ["Title", "Author", "Content"]
 
 
-admin.site.register(Post)
+admin.site.register(Post, Admin_PostCustomizations)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
@@ -22,3 +23,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.Title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+        args= [self.Published_Date,
+        self.Slug,
+        self.id]
+        )
