98813b0ba42a54b6d6cdb8ee32f5aa6e9e20b366
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-12 18:14:28 +0430

    set up category bar

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name= 'blog'
 
 urlpatterns = [
     path('', blog_home, name='home'),
+    path('category/<str:cat>', blog_home, name='category'),
     path('<int:pid>', blog_post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from blog.models import Post, Category
 
 
 # Create your views here.
-def blog_home(request):
-    posts= Post.objects.filter(Status = True)
-    Context = {'posts': posts}
+def blog_home(request, **kwargs):
+    if kwargs.get('cat') != None:
+        posts= Post.objects.filter(Status = True, Category__Name = kwargs["cat"])
+    else:
+        posts= Post.objects.filter(Status = True)
+
+    # get Categories
+    Categories= Category.objects.all()[:10]
+    Context = {'posts': posts, 'category': Categories}
     return render(request, 'blog/blog.html', Context)
 
 def blog_post(request, pid):
