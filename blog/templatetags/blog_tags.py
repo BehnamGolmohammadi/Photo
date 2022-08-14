d97f1edff3f4acf9d3e5264dd7a3cb456be70a30
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-14 09:25:59 +0430

    Set up blog Album in main home page
    use inclusion tag by custom tag and then put the html page into main page

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -13,3 +13,14 @@ def LatestPosts():
 @register.filter
 def Shorter_Content(Content, UpTo = 10):
     return ' '.join(Content.split()[:UpTo]) + ' ...'
+
+@register.inclusion_tag('website/Website-Album.html')
+def Album():
+    posts= Post.objects.filter(Status = True).order_by('-Published_Date')
+    categories = Category.objects.all()[:10]
+    Category_Dict= dict()
+
+    for category in categories:
+        Category_Dict[category] = list(posts.filter(Category = category))
+    Context = {'Category_Dict': Category_Dict}
+    return Context
