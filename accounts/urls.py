968d51dfb3af67c5aa85d3d14d9c69e10ce54f44
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-20 18:00:49 +0430

    set up login and setup some of signup page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from accounts.views import *
+from django.contrib.auth import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('login', accounts_login, name= 'login'),
+    path('logout', accounts_logout, name= 'logout'),
     path('signup', accounts_signup, name = 'signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate as auth , login, logout
+from django.contrib import messages
 
 # Create your views here.
 def accounts_signup(request):
+    if request.user.is_authenticated:
+        msg= f'{request.user.first_name}, you have been logged in as {request.user.username}.'
+        messages.success(request, msg)
+        return redirect('/')
     return render(request, 'registration/signup.html')
+
+def accounts_login(request):
+    if request.user.is_authenticated:
+        msg= f'{request.user.first_name}, you have been logged in as {request.user.username}.'
+        messages.success(request, msg)
+        return redirect('/')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            msg= f'Hi {request.user.get_full_name()}, You\'ve been logged in successfully.'
+            messages.success(request, msg)
+            return redirect(request.POST['next'])
+        else:
+            msg= f'Username or password or both are not correct'
+            messages.success(request, msg)
+            return redirect(request.POST['next'])
+    else:
+        return render(request, 'registration/login.html')
+
+def accounts_logout(request):
+    if request.user.is_authenticated:
+        msg= f'Bye {request.user.get_full_name()}, You logged out successfully.'
+        logout(request)
+        messages.info(request, msg)
+        return redirect('/')
+    else:
+        msg= f'Dear visitor, you\'re not logged in yet, if you want you can login or singup.'
+        messages.warning(request, msg)
+        return render(request, 'registration/login.html')
