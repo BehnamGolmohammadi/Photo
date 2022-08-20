a82ab2a0f6a820bb322b0ccf12d6c5d92269e247
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-21 01:23:49 +0430

    Setup sign up page also some edit

---
+++ accounts/froms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields= ['username', 'password', 'email', 'first_name', 'last_name']
+    
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An user with this email already exists!")
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate as auth , login, logout
 from django.contrib import messages
-
+from accounts.froms import SignupForm
 # Create your views here.
 def accounts_signup(request):
     if request.user.is_authenticated:
         msg= f'{request.user.first_name}, you have been logged in as {request.user.username}.'
         messages.success(request, msg)
         return redirect('/')
-    return render(request, 'registration/signup.html')
+    elif request.method == 'POST':
+        Form = SignupForm(request.POST)
+        Next = request.POST.get('next')
+        if Form.is_valid():
+            pass1 = Form.cleaned_data.get('password')
+            pass2 = request.POST['password2']
+            if pass1 == pass2:
+                user= Form.save()
+                login(request, user)
+                msg= f'Welcome {request.user.get_full_name()}, You\'ve been signed up successfully.'
+                messages.success(request, msg)
+                if Next:
+                    return redirect(Next)
+                else:
+                    return redirect('/')
+            else:
+                msg= f'Dear visitor your entired passwords do not match or other fields are not filled correctly'
+                messages.warning(request, msg)
+                if Next :
+                    return redirect(f"/accounts/signup?next={Next}")
+                else:
+                    return redirect('/accounts/signup')
+        else:
+            msg= f'Dear visitor your entired information are not valid! Maybe your email is submitted before.'
+            messages.warning(request, msg)
+            if Next:
+                return redirect(f"/accounts/signup?next={Next}")
+            else:
+                return redirect('/accounts/signup')
+    else:
+        Form = SignupForm()
+        return render(request, 'registration/signup.html', {'Form': Form})
 
 def accounts_login(request):
     if request.user.is_authenticated:
