892172ae19fe4a17c6f5ee3353849487b16ceec0
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-08-25 08:54:02 +0430

    Update settings.py
    change sending email address

--- photo/settings.py
+++ photo/settings.py
@@ -171,5 +171,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gt.bg.78@gmail.com'
-EMAIL_HOST_PASSWORD = 'wyxxlkirhizsfptn'
+EMAIL_HOST_USER = 'linuxbehnam@gmail.com'
+EMAIL_HOST_PASSWORD = 'rkgbyccferhnucfc'
