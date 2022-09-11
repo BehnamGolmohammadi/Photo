2e8cf27ab8696bf48609a9567dbd7cd0a0d33ba6
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-09-11 10:35:11 +0430

    Update views.py
    some edit for verification and debug some error or bugs

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,39 @@ from django.contrib.auth import authenticate as auth , login, logout
 from django.contrib import messages
 from accounts.froms import SignupForm
 # Create your views here.
+
+# Function to validate the password
+def password_check(request, passwd):
+      
+    SpecialSym =['$', '@', '#', '%', '.', '*']
+    val = True
+      
+    if len(passwd) < 6:
+        messages.add_message(request, 30, 'length should be at least 6')
+        val = False
+          
+    if len(passwd) > 20:
+        messages.add_message(request, 30, 'length should be not be greater than 20')
+        val = False
+          
+    if not any(char.isdigit() for char in passwd):
+        messages.add_message(request, 30, 'Password should have at least one numeral')
+        val = False
+          
+    if not any(char.isupper() for char in passwd):
+        messages.add_message(request, 30, 'Password should have at least one uppercase letter')
+        val = False
+          
+    if not any(char.islower() for char in passwd):
+        messages.add_message(request, 30, 'Password should have at least one lowercase letter')
+        val = False
+          
+    if not any(char in SpecialSym for char in passwd):
+        messages.add_message(request, 30, 'Password should have at least one of the symbols [$ @ # % . *]')
+        val = False
+    if val:
+        return val
+
 def accounts_signup(request):
     if request.user.is_authenticated:
         msg= f'{request.user.first_name}, you have been logged in as {request.user.username}.'
@@ -14,7 +47,12 @@ def accounts_signup(request):
         if Form.is_valid():
             pass1 = Form.cleaned_data.get('password')
             pass2 = request.POST['password2']
-            if pass1 == pass2:
+            if not (password_check(request, pass1)):
+                if Next :
+                    return redirect(f"/accounts/signup?next={Next}")
+                else:
+                    return redirect('/accounts/signup')
+            elif pass1 == pass2:
                 user= Form.save()
                 login(request, user)
                 msg= f'Welcome {request.user.get_full_name()}, You\'ve been signed up successfully.'
@@ -57,8 +95,11 @@ def accounts_login(request):
             return redirect(request.POST['next'])
         else:
             msg= f'Username or password or both are not correct'
-            messages.success(request, msg)
-            return redirect(request.POST['next'])
+            messages.error(request, msg)
+            if request.POST['next']:
+                return redirect(f"/accounts/login?next={request.POST['next']}")
+            else:
+                return redirect('/accounts/login')
     else:
         return render(request, 'registration/login.html')
 
