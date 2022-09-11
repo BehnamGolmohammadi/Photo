4cce268b901e6d3a35dea4de5201a5b5b27767e7
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-09-11 12:22:25 +0430

    Update settings.py
    a copy of db setting on server

--- photo/settings.py
+++ photo/settings.py
@@ -94,6 +94,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '*********_Photo',
+#         'USER': '*********_Behnam',
+#         'PASSWORD': '*******************',
+#         'HOST': 'localhost',
+#         'PORT': '****',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
